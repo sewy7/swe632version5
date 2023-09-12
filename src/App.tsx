de6b940b610708e6a999647a3d27b18a3857c431
sewy7 <115030520+sewy7@users.noreply.github.com>
2023-09-12 18:39:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,33 @@
 import React, { useState, useEffect } from "react";
-import type { User, Post } from "./MessageContext";
-import {
-  Button,
-  Container,
-  Box,
-  TextField,
-  Typography,
-  List,
-  ListItem,
-  Avatar,
-  Divider,
-  Paper,
-} from "@mui/material";
-import { Send, ThumbUp, ThumbDown } from "@mui/icons-material";
+import type { User, Post} from './MessageContext';
+import { Button, Container, Box, TextField, Typography, List, ListItem, Avatar, Divider, Paper,} from "@mui/material";
+import { Send } from "@mui/icons-material";
+import jsonData from "./data.json";
+
 
 function App() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [newPost, setNewPost] = useState<string>("");
-  const [users, setUsers] = useState<User[]>([]);
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
-
-  const [upvotes, setUpvotes] = useState<number[]>([]);
-  const [downvotes, setDownvotes] = useState<number[]>([]);
-
-  useEffect(() => {
-    fetch("./data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data.users);
-        setPosts(data.posts);
-        setCurrentUser(data.users[0]);
-        setUpvotes(new Array(data.posts.length).fill(0));
-        setDownvotes(new Array(data.posts.length).fill(0));
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  const [users, setUsers] = useState(jsonData.users);
+  const [posts, setPosts] = useState(jsonData.posts);
+  const [newPost, setNewPost] = useState("");
+  const [currentUser, setCurrentUser] = useState(users[0]);
 
   const addPost = () => {
     if (newPost.trim() !== "" && currentUser) {
-      const newId = posts.length + 1;
-      const newPostObj: Post = {
-        id: newId,
+      const newPostObj = {
         user: currentUser,
         content: newPost,
       };
       setPosts((prevPosts) => [...prevPosts, newPostObj]);
       setNewPost("");
-      setUpvotes((prevUpvotes) => [...prevUpvotes, 0]);
-      setDownvotes((prevDownvotes) => [...prevDownvotes, 0]);
     }
   };
 
   const switchUser = () => {
     if (currentUser) {
-      const currentIndex = users.findIndex((user) => user.id === currentUser.id);
+      const currentIndex = users.indexOf(currentUser);
       const nextIndex = (currentIndex + 1) % users.length;
       setCurrentUser(users[nextIndex]);
     }
-  };
-
-  const handleUpvote = (postId: number) => {
-    const newUpvotes = [...upvotes];
-    newUpvotes[postId - 1]++;
-    setUpvotes(newUpvotes);
-  };
-
-  const handleDownvote = (postId: number) => {
-    const newDownvotes = [...downvotes];
-    newDownvotes[postId - 1]++;
-    setDownvotes(newDownvotes);
   };
 
   return (
@@ -87,33 +46,13 @@ function App() {
         <Paper elevation={3}>
           <List>
             {posts.map((post, index) => (
-              <ListItem key={post.id}>
-                <Avatar alt={post.user.name} />
+              <ListItem key={index}>
+                <Avatar>{post.user[0]}</Avatar>
                 <Box ml={2}>
                   <Typography variant="subtitle1" fontWeight="bold">
-                    {post.user.name}
+                    {post.user}
                   </Typography>
                   <Typography variant="body1">{post.content}</Typography>
-                </Box>
-                <Box ml={2}>
-                  <Button
-                    variant="text"
-                    color="primary"
-                    startIcon={<ThumbUp />}
-                    onClick={() => handleUpvote(post.id)}
-                  >
-                    Upvote ({upvotes[index]})
-                  </Button>
-                </Box>
-                <Box ml={2}>
-                  <Button
-                    variant="text"
-                    color="secondary"
-                    startIcon={<ThumbDown />}
-                    onClick={() => handleDownvote(post.id)}
-                  >
-                    Downvote ({downvotes[index]})
-                  </Button>
                 </Box>
               </ListItem>
             ))}
@@ -124,7 +63,7 @@ function App() {
         <Box display="flex" alignItems="center">
           <TextField
             fullWidth
-            label={`Add a new post as ${currentUser?.name || ""}`}
+            label={`Add a new post as ${currentUser}`}
             variant="outlined"
             value={newPost}
             onChange={(e) => setNewPost(e.target.value)}
